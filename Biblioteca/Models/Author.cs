982f0201a54e5d8d1b23d622a633d4fb6d07fd4d
MIRELA APARECIDA NEVES CALO <mirela.ancalo@senacsp.edu.br>
2023-07-28 17:25:23 -0300

    Adicionado o EFe criados os models de author e publisher

---
+++ Biblioteca/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Biblioteca.Models
+{
+	[Table("Authors")]
+		public class Author
+	{
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Column("AuthorName")]
+        public string? Name { get; set; }
+        [MaxLength(80), MinLength(4)]
+        public string? Site { get; set; }
+    }
+}
