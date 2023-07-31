043c54ae5ee473950fe12b52545e7198f87f1879
MIRELA APARECIDA NEVES CALO <mirela.ancalo@senacsp.edu.br>
2023-07-31 17:28:26 -0300

    Criação do migration

--- Biblioteca/Data/BibliotecaContext.cs
+++ Biblioteca/Data/BibliotecaContext.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Models;
 
 namespace Biblioteca.Data
 {
-	public class BibliotecaContext: DbContext
-	{
-		public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
-		{
-
-		}
-
-	}
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext (DbContextOptions<BibliotecaContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book> Book { get; set; } = default!;
+        public DbSet<Publisher> Publishe { get; set; } = default!;
+        public DbSet<Author> Author { get; set; } = default!;      
+    }
 }
